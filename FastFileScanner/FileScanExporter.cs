b4fdb9e935aa1fb7c95552f396232cc15492c3ac
zhouguoqing <zhouguoqing.eric@gmail.com>
2021-01-24 20:19:03 +0800

    增加文件扫描类

---
+++ FastFileScanner/FileScanExporter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFileScanner
+{
+    class FileScanExporter
+    {
+        public static string ExportCSV(string filePath, List<ScanFileInfo> checkResults)
+        {
+            var csvFile = Path.Combine(filePath, "文件扫描结果-" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv");
+
+            StringBuilder builder = new StringBuilder();
+            List<string> columnNames = new List<string>();
+            if (checkResults.FirstOrDefault() is ScanFileInfo)
+            {
+                columnNames.Add("文件名称");
+                columnNames.Add("文件大小");
+                columnNames.Add("文件类型");
+                columnNames.Add("文件最后修改时间");
+                columnNames.Add("文件所在目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+                columnNames.Add("子目录");
+            }
+
+            builder.Append(string.Join(",", columnNames.ToArray())).Append("\n");
+
+            var rows = new List<string>();
+            foreach (var result in checkResults)
+            {
+                var currentRow = new List<string>() { result.FileName, result.FileSize.ToString(), result.Extension, result.LastModifyTime.ToString(),
+                    result.FilePath,result.SubDir1, result.SubDir2,result.SubDir3,result.SubDir4,result.SubDir5,result.SubDir6,result.SubDir7,result.SubDir8,result.SubDir9,result.SubDir10,result.SubDir11,result.SubDir12 };
+                rows.Add(string.Join(",", currentRow.ToArray()));
+            }
+
+            builder.Append(string.Join("\n", rows.ToArray()));
+
+            byte[] CSVBinaryData = Encoding.UTF8.GetBytes(builder.ToString());
+            CSVBinaryData = Encoding.UTF8.GetPreamble().Concat(CSVBinaryData).ToArray();
+            File.WriteAllBytes(csvFile, CSVBinaryData);
+
+            return csvFile;
+        }
+    }
+}
---
+++ FastFileScanner/ScanFileInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFileScanner
+{
+    class ScanFileInfo
+    {
+        public string FileName { get; set; }
+
+        public long FileSize { get; set; }
+
+        public string Extension { get; set; }
+
+        public DateTime LastModifyTime { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string SubDir1 { get; set; } = "";
+
+        public string SubDir2 { get; set; } = "";
+
+        public string SubDir3 { get; set; } = "";
+
+        public string SubDir4 { get; set; } = "";
+
+        public string SubDir5 { get; set; } = "";
+
+        public string SubDir6 { get; set; } = "";
+
+        public string SubDir7 { get; set; } = "";
+
+        public string SubDir8 { get; set; } = "";
+
+        public string SubDir9 { get; set; } = "";
+
+        public string SubDir10 { get; set; } = "";
+
+        public string SubDir11 { get; set; } = "";
+
+        public string SubDir12 { get; set; } = "";
+
+        public string FileMd5 { get; set; } = "";
+
+        public string ExceptionMsg { get; set; }
+    }
+}
